4d14500d38e37b0bc1867dd17f4bbc3488022e44
anthonymartinezacc <123520843+anthonymartinezacc@users.noreply.github.com>
2023-03-25 18:43:42 -0500

    Add files via upload

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,17 @@
-//h
+using std::string;
+
+bool game_over();
+
+void start_game(string &first_player);
+
+void mark_board(int position);
+
+string get_player(); //const;
+
+void display_board(); //const;
+
+void set_next_player();
+
+bool check_board_full();
+
+void clear_board();
